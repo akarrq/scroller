3b32175fed97cf16c3486bd7f2c9407bb6460ea1
Błażej Plichta <b.plichta96@gmail.com>
2022-11-10 15:58:32 +0100

    scroll implementation

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   const rootElement = document.querySelector("#root");
   const sections = document.querySelectorAll("section");
+  let currentSectionIndex = 0;
+  let isThrottled = false;
 
-  document.addEventListener("wheel", (event) => {
-    console.log(event.wheelDelta);
+  document.addEventListener("wheel", (e) => {
+    if (isThrottled) return;
+    isThrottled = true;
+
+    setTimeout(() => {
+      isThrottled = false;
+    }, 500);
+
+    const direction = e.wheelDelta < 0 ? 1 : -1;
+
+    if (direction === 1) {
+      const isLastSection = currentSectionIndex === sections.length - 1;
+      if (isLastSection) return;
+    } else if (direction === -1) {
+      const isFirstSection = currentSectionIndex === 0;
+      if (isFirstSection) return;
+    }
+
+    currentSectionIndex = currentSectionIndex + direction;
+
+    sections[currentSectionIndex].scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
   });
 });
