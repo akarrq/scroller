0c0864975a2f4c1894b8540bd93741667635a783
Błażej Plichta <b.plichta96@gmail.com>
2022-11-15 18:06:48 +0100

    add keyboard suport

--- script.js
+++ script.js
@@ -4,4 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
   document.addEventListener("wheel", (e) => scroller.listenScroll(e));
   document.addEventListener("swipeUp", () => scroller.scroll(1));
   document.addEventListener("swipeDown", () => scroller.scroll(-1));
+  document.addEventListener("keydown", (e) => {
+    switch (e.key) {
+      case "ArrowDown":
+        return scroller.scroll(1);
+      case "ArrowUp":
+        return scroller.scroll(-1);
+      default:
+        return;
+    }
+  });
 });
